5ff2861dfd6e4696988d55b5bd8ef73a5ff3bfb9
Andrew Thomas <ajthomas145@gmail.com>
2021-09-15 11:43:38 +0100

    second commit

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -9,4 +9,15 @@ class TestPub(unittest.TestCase):
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
 
-    
+    def test_pub_has_cash(self):
+        self.assertEqual(100.00, self.pub.till)
+
+    def test_pub_has_drinks(self):
+        self.assertEqual(3, self.pub.drinks_list())
+
+    def test_add_cash_to_till(self):
+        self.assertEqual(110.00, self.pub.add_cash_to_till(price_of_drink))
+
+    def test_remove_drink_from_list(self):
+        self.pub.remove_drink_from_list(drink_sold)
+        self.assertEqual(2, self.pub.pub.drinks_list())
