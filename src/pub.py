e56b35831ae09e609f9351bb918bd5d882b2c35a
Andrew Thomas <ajthomas145@gmail.com>
2021-09-15 13:33:08 +0100

    third commit

--- src/pub.py
+++ src/pub.py
@@ -3,3 +3,10 @@ class Pub:
     def __init__(self, input_name, input_till):
         self.name = input_name
         self.till = input_till
+        self.drinks_list = []
+
+    def number_of_drinks(self):
+        return len(self.drinks_list)
+
+    def add_cash_to_till(self, price_of_drink):
+        self.till += price_of_drink
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,10 +1,12 @@
 import unittest
 from src.pub import Pub
+from src.drink import Drink
 
 class TestPub(unittest.TestCase):
 
     def setUp(self):
         self.pub = Pub("The Prancing Pony", 100.00)
+        self.drink = Drink("beer", 5.00)
 
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
@@ -12,12 +14,22 @@ class TestPub(unittest.TestCase):
     def test_pub_has_cash(self):
         self.assertEqual(100.00, self.pub.till)
 
+    # @unittest.skip("delete this line to run the test")
     def test_pub_has_drinks(self):
-        self.assertEqual(3, self.pub.drinks_list())
+        self.assertEqual(0, self.pub.number_of_drinks())
 
+    # @unittest.skip("delete this line to run the test")
     def test_add_cash_to_till(self):
-        self.assertEqual(110.00, self.pub.add_cash_to_till(price_of_drink))
+        self.pub.add_cash_to_till(5.00)
+        self.assertEqual(105.00, self.pub.till)
 
+    @unittest.skip("delete this line to run the test")
+    def test_add_drink_from_list(self):
+        self.pub.add_drink_to_list(self.drink)
+        self.assertEqual(1, self.pub.number_of_drinks())
+
+    @unittest.skip("delete this line to run the test")
     def test_remove_drink_from_list(self):
-        self.pub.remove_drink_from_list(drink_sold)
-        self.assertEqual(2, self.pub.pub.drinks_list())
+        self.pub.add_drink_to_list(self.drink)
+        self.pub.remove_drink_from_list(self.drink)
+        self.assertEqual(0, self.pub.number_of_drinks())
